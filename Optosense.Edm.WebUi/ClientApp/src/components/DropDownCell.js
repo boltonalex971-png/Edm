1472befa148bc3ed5326bdf02c87c1f6c0725c98
Alexander Bolotin <alexander.bolotin@microprojects.ru>
2021-01-13 09:18:14 +0000

    Merged PR 276: Fix dropdown cell in relation tables
    Fix dropdown cell in relation tables
    
    - Shows 'DELETED' if no corresponding value exists
    - Takes 'fieldName' param to select text from item

--- Optosense.Edm.WebUi/ClientApp/src/components/DropDownCell.js
+++ Optosense.Edm.WebUi/ClientApp/src/components/DropDownCell.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React
+    from 'react';
 import { DropDownList } from '@progress/kendo-react-dropdowns';
 import { GridCell } from '@progress/kendo-react-grid';
 
-export function dropDownCell(getData, key, text) {
+export function dropDownCell(getData, key, text, fieldName) {
     return class extends GridCell {
         handleChange = (e) => {
             this.props.onChange({
@@ -14,57 +15,30 @@ export function dropDownCell(getData, key, text) {
         }
 
         render() {
-            const list = getData();
+            let content;
             const { dataItem, field } = this.props;
             const dataValue = dataItem[field];
+            const dataName = dataItem[fieldName];
+            if (dataItem.inEdit || !fieldName) {
+                const list = getData();
+                const value = list.find(c => c[key] === dataValue);
+                content = dataItem.inEdit ?
+                    < DropDownList
+                        onChange={this.handleChange}
+                        value={value}
+                        data={list}
+                        textField={text}
+                        dataItemKey={key}
+                    /> : (value && value[text] || '- DELETED -').toString();
+            } else if (fieldName) {
+                content = dataItem[fieldName];
+            } 
+
             return (
                 <td style={{ whiteSpace: 'nowrap' }}>
-                    {dataItem.inEdit ? (
-                        <DropDownList
-                            onChange={this.handleChange}
-                            value={list.find(c => c[key] === dataValue)}
-                            data={list}
-                            textField={text}
-                            dataItemKey={key}
-                        />
-                    ) : (
-                            list.find(c => c[key] === dataValue)[text].toString()
-                        )}
+                    {content}
                 </td>
             )
         }
     }
 }
-
-// export class DropDownCell extends React.Component {
-
-//     handleChange = (e) => {
-//         this.props.onChange({
-//             dataItem: this.props.dataItem,
-//             field: this.props.field,
-//             syntheticEvent: e.syntheticEvent,
-//             value: e.target.value.value
-//         });
-//     }
-
-//     render() {
-//         const { dataItem, field } = this.props;
-//         const dataValue = dataItem[field] === null ? '' : dataItem[field];
-
-//         return (
-//             <td>
-//                 {dataItem.inEdit ? (
-//                     <DropDownList
-//                         style={{ width: "100px" }}
-//                         onChange={this.handleChange}
-//                         value={this.localizedData.find(c => c.value === dataValue)}
-//                         data={this.localizedData}
-//                         textField="text"
-//                     />
-//                 ) : (
-//                         dataValue.toString()
-//                     )}
-//             </td>
-//         );
-//     }
-// }
