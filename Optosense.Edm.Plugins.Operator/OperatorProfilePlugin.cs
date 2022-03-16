99e1397b2c97941970e5e062762844b1e53041f0
Alexander Bolotin <alexander.bolotin@microprojects.ru>
2022-03-16 19:10:54 +0000

    Merged PR 384: Add ResponseTime parameter for Operator profile plugin
    Add ResponseTime parameter for Operator profile plugin

--- Optosense.Edm.Plugins.Operator/OperatorProfilePlugin.cs
+++ Optosense.Edm.Plugins.Operator/OperatorProfilePlugin.cs
@@ -22,7 +22,7 @@ namespace Optosense.Edm.Profiles.Operator
             var parameters = profile
                 .SelectMany(p => JsonConvert.DeserializeObject<IEnumerable<string>>(p.Parameters ?? "[]"))
                 .Distinct(); 
-            return parameters;
+            return parameters.Prepend("ResponseTime");
         }
 
     }
