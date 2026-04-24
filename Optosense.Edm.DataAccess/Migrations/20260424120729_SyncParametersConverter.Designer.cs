5de1d4b7372abf16305ff930ce30a98ab7e25205
Alex Bolton <boltonalex971@gmail.com>
2026-04-24 17:22:47 +0300

    Fix bundle migration handling (#4)
    Co-authored-by: Alexander Bolotin <alexander.bolotin@microprojects.ru>

--- Optosense.Edm.DataAccess/Migrations/20260424120729_SyncParametersConverter.Designer.cs
+++ Optosense.Edm.DataAccess/Migrations/20260424120729_SyncParametersConverter.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Optosense.Edm.Persistence;
 
@@ -11,9 +12,11 @@ using Optosense.Edm.Persistence;
 namespace Optosense.Edm.DataAccess.Migrations
 {
     [DbContext(typeof(EdmContext))]
-    partial class EdmContextModelSnapshot : ModelSnapshot
+    [Migration("20260424120729_SyncParametersConverter")]
+    partial class SyncParametersConverter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
