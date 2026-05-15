83ef11eeb04fb331280731e5fb94eb84da3b685e
Alexander Bolotin <alexander.bolotin@microprojects.ru>
2026-05-15 15:21:30 +0300

    Render supply components as tares

--- Microprojects.Edm.Ui.Logistics/Ui/src/components/supplies/SupplyComponentTab.tsx
+++ Microprojects.Edm.Ui.Logistics/Ui/src/components/supplies/SupplyComponentTab.tsx
@@ -1,11 +1,11 @@
 import Api from '@features/api/api'
-import { RelationTable } from '@logistics/components/RelationTable'
 import { BatchItemCreate } from '@logistics/components/items/BatchItemCreate'
 import { ItemDetail } from '@logistics/components/items/ItemDetail'
+import { TareDetail } from '@logistics/components/tare/TareDetail'
+import { TareItemsPanel } from '@logistics/components/tare/TareItemsPanel'
 import type { UUID } from '@logistics/data/types'
-import { Column as GridColumn } from '@microprojects/edm-components/components'
 import { AddOutlined as AddIcon } from '@mui/icons-material'
-import { Button as MuiButton } from '@mui/material'
+import { Box, Button as MuiButton } from '@mui/material'
 
 type SupplyComponentTabProps = {
     id: UUID
@@ -19,47 +19,55 @@ export function SupplyComponentTab({
     onDetailSelected,
 }: SupplyComponentTabProps) {
     return (
-        <RelationTable
+        <TareItemsPanel
             api={`${api}/${id}/items`}
-            readonly
-            onRowSelected={(row) =>
+            supplyId={id}
+            toolbar={
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <MuiButton
+                        variant="contained"
+                        size="small"
+                        startIcon={<AddIcon />}
+                        onClick={() =>
+                            onDetailSelected?.(
+                                <BatchItemCreate
+                                    supplyId={id}
+                                    onClose={() =>
+                                        onDetailSelected?.(undefined)
+                                    }
+                                />,
+                            )
+                        }
+                        sx={{
+                            marginBottom: '0.5rem'
+                        }}
+                    >
+                        Add items
+                    </MuiButton>
+                </Box>
+            }
+            onTareClick={(group) => {
+                onDetailSelected?.(
+                    <TareDetail
+                        tareId={group.tare.id}
+                        tare={group.tare}
+                        items={group.items}
+                        label={group.tare.barcode}
+                        onClose={() => onDetailSelected?.(undefined)}
+                    />,
+                )
+            }}
+            onItemClick={(item) => {
                 onDetailSelected?.(
                     <ItemDetail
                         readonly={true}
-                        id={row.id}
+                        id={item.id}
                         api={Api.items}
                         type="item"
                         onClose={() => onDetailSelected?.(undefined)}
                     />,
                 )
-            }
-            toolbarEnd={
-                <MuiButton
-                    variant="contained"
-                    size="small"
-                    startIcon={<AddIcon />}
-                    onClick={() =>
-                        onDetailSelected?.(
-                            <BatchItemCreate
-                                supplyId={id}
-                                onClose={() => onDetailSelected?.(undefined)}
-                            />,
-                        )
-                    }
-                >
-                    Add items
-                </MuiButton>
-            }
-        >
-            <GridColumn field="nomenclatureName" title="Nomenclature" />
-            <GridColumn field="serialNo" title="Serial No" width="120" />
-            <GridColumn field="quantity" title="Quantity" width="100" />
-            <GridColumn field="tareBarcode" title="Tare" width="140" />
-            <GridColumn
-                field="tareTareTypeName"
-                title="Tare Type"
-                width="160"
-            />
-        </RelationTable>
+            }}
+        />
     )
 }
